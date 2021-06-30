ae57bd2d2c9f15ce7707f0b80631e9744ea5a8a0
juvenalculino <b831381@gmail.com>
2021-06-30 09:05:56 -0300

    subindo aplicaçao

---
+++ posts/views.py
@@ -0,0 +1,112 @@
+from django.shortcuts import render, redirect
+from django.views.generic.list import ListView
+from django.views.generic.edit import UpdateView
+from .models import Post
+# para funcionar com annotate
+from django.db.models import Q, Count, Case, When
+# Importando os forms criados em comentários
+from comentarios.forms import FormComentario
+# Importando model do comentário
+from comentarios.models import Comentario
+# Imporrando mensagens de erro
+from django.contrib import messages
+
+# Create your views here.
+class PostIndex(ListView):
+    model = Post
+    template_name = 'posts/index.html'
+    paginate_by = 6
+    # Utilizando o loop do for do index do post abaixo estamos setando o oject name
+    context_object_name = 'posts'
+
+    # sobrescrevendo método
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # Ordenando de forma decrescente
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        qs = qs.annotate(
+            numero_comentarios=Count(
+                Case(
+                    When(comentario__publicado_comentario=True, then=1)
+                )
+            )
+        )
+        return qs
+
+
+class PostBusca(PostIndex):
+    template_name = 'posts/post_busca.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+
+        termo = self.request.GET.get('termo')
+
+        if not termo:
+            return qs
+
+        qs = qs.filter(
+            Q(titulo_post__icontains=termo) | 
+            Q(autor_post__first_name__iexact=termo) | 
+            Q(conteudo_post__icontains=termo) | 
+            Q(excerto_post__icontains=termo) | 
+            Q(categoria_post__nome_cat__iexact=termo)
+        )
+
+        return qs
+
+
+class PostCategoria(PostIndex):
+    template_name = 'posts/post_categoria.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        # Ordenando de forma decrescente
+        qs = qs.order_by('-id').filter(publicado_post=True)
+        
+        
+        categoria = self.kwargs.get('categoria', None)
+
+        if not categoria:
+            return qs
+        # Buscando por categoria específica
+        qs = qs.filter(categoria_post__nome_cat__iexact=categoria)        
+             
+        return qs
+
+
+class PostDetalhes(UpdateView):
+    template_name = 'posts/post_detalhes.html'
+    model = Post
+    # Teremos que criar um form class no app comentários = forms.py
+    form_class = FormComentario 
+
+    # Passando nome do contexto p reconhecer no post_detalhes
+    context_object_name = 'post'
+
+    # sobrescrevendo método para poder exibir comentários no post_detalhes.html
+    def get_context_data(self, **kwargs):
+        contexto = super().get_context_data(**kwargs)
+        post = self.get_object()
+        comentarios = Comentario.objects.filter(publicado_comentario=True, post_comentario=post.id)
+        contexto['comentarios'] = comentarios
+
+        return contexto
+
+
+
+    # Se o comentário for válido re-definimos o método
+    def form_valid(self, form):
+        post = self.get_object()
+        comentario = Comentario(**form.cleaned_data)
+        comentario.post_comentario = post
+
+        if self.request.user.is_authenticated:
+            comentario.usuario_comentario = self.request.user
+
+        comentario.save()
+        messages.success(self.request, 'Comentário enviado com sucesso!')
+        #redirecionando a página
+        return redirect('post_detalhes', pk=post.id)
+
+        
