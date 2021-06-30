c1df82dd8e7b2716e6a16996d0dd6a6c300327e7
juvenalculino <b831381@gmail.com>
2021-06-30 11:24:22 -0300

    configurando models e url do posts

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from . import settings
+from . import static
 
 urlpatterns = [
     path('', views.PostIndex.as_view(), name='index'),
@@ -7,3 +9,5 @@ urlpatterns = [
     path('busca/', views.PostBusca.as_view(), name='post_busca'),
     path('post/<int:pk>', views.PostDetalhes.as_view(), name='post_detalhes'),
 ]
+
+urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
